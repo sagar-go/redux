11ff1f2f1814c8a24dce3bfc6fe78f6eae4972ec
Sagar <sagarrbarthwal@gmail.com>
2022-06-17 12:30:06 +0530

    riririr

--- redux/src/compo/Calls.jsx
+++ redux/src/compo/Calls.jsx
@@ -1,7 +1,7 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 
 export const getUsers = createAsyncThunk(
-  "myyuser user2",
+  "myyuser user245455",
   async (dispatch, getState) => {
     return await fetch("https://fakestoreapi.com/products/").then((res) =>
       res.json()
